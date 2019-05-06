b2cb9b495dc22d2ed21dd34f3b28550a2440676b
DoorSupporter <jiwon3346@gmail.com>
2019-05-06 13:54:56 +0900

    remove between Dollar&Franc and Money

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -7,14 +7,14 @@ public class Money {
         this.currency = currency;
     }
 
-    static Dollar dollar(int amount) {
-        return new Dollar(amount, "USD");
+    static Money dollar(int amount) {
+        return new Money(amount, "USD");
     }
 
-    static Franc franc(int amount) {
-        return new Franc(amount, "CHF");
+    static Money franc(int amount) {
+        return new Money(amount, "CHF");
     }
-    
+
     Money times(int multiplier){
         return new Money(amount * multiplier,currency);
     }
@@ -32,6 +32,4 @@ public class Money {
         Money money = (Money) object;
         return money.amount == this.amount && currency().equals(money.currency());
     }
-
-
 }
