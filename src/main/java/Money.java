3a22851680ef75207d3e7c59e71e66d61c6e5403
DoorSupporter <jiwon3346@gmail.com>
2019-05-06 14:15:09 +0900

    add plus method

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,4 +1,4 @@
-public class Money {
+public class Money implements Expression {
     protected int amount;
     protected String currency;
 
@@ -23,6 +23,11 @@ public class Money {
         return currency;
     }
 
+    Expression plus (Money addend) {
+        return new Money(amount + addend.amount, currency);
+    }
+
+    @Override
     public String toString() {
         return amount + " " + currency;
     }
